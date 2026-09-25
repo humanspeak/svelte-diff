2d9d0fe5880ba7f62fddc8646f220bc6f272a3bb
Jason Kummerl <jason.h@kummerl.com>
2026-09-25 11:03:35 -0400

    fix(docs): serve a crawlable branded PNG favicon

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import '../app.css'
     import { RootLayout } from '@humanspeak/docs-kit'
-    import favicon from '$lib/assets/logo.svg'
     import { docsConfig } from '$lib/docs-config'
     import githubStats from '$lib/github-stats.json'
 
     const { children } = $props()
 </script>
 
-<RootLayout config={docsConfig} {favicon} stars={githubStats.stars}>
+<RootLayout config={docsConfig} favicon="/favicon.png" stars={githubStats.stars}>
     {@render children?.()}
 </RootLayout>
